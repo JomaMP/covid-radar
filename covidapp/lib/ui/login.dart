550c43dfdeeb33cc0de2a34395c3bcd2ad5132d0
JoohnSnow <torresisaac510@gmail.com>
2021-07-30 14:52:56 -0500

    Intente hacer funcionar el login

--- covidapp/lib/ui/login.dart
+++ covidapp/lib/ui/login.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:covidapp/ui/home.dart';
 import 'package:covidapp/ui/registro.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 class Login extends StatelessWidget {
   void registroPress(BuildContext context) {
@@ -51,7 +55,7 @@ class Login extends StatelessWidget {
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(10.0)),
                                 ))),
-                        SizedBox(height: 15),
+                        SizedBox(height: 15)  ,
                         SizedBox(height: 35),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 60),
@@ -94,3 +98,41 @@ class Login extends StatelessWidget {
     );
   }
 }
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({ Key? key }) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController controllerUser = new TextEditingController();
+  TextEditingController controllerPassword = new TextEditingController();
+
+  String mensaje = "";
+  
+
+  Future obtenerUsuario() async {
+    var url = "localhost/pruebas/obtenerUsuario.php";
+    final response = await http.post(Uri.parse(url), body: {
+      "usuario": controllerUser.text,
+      "contrasena": controllerPassword.text
+    });
+
+    if(response.body == "CORRECTO") {
+      
+    } else if(response.body == "ERROR") {
+      
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
